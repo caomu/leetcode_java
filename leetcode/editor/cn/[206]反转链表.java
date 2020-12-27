ad40dccb02ce636862c1322ded1ff76fdc9fe6d4
caomu <caomu@users.noreply.github.com>
2020-12-27 19:51:32 +0900

    Initial commit

---
+++ leetcode/editor/cn/[206]反转链表.java
@@ -0,0 +1,41 @@
+//反转一个单链表。 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 进阶: 
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+// Related Topics 链表 
+// 👍 1401 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	public ListNode reverseList(ListNode head) {
+		ListNode prev = null;
+		ListNode curr = head;
+		while (curr.next != null) {
+			System.out.println(curr.val);
+			prev = curr;
+			curr = curr.next;
+			System.out.println(head.next.val);
+
+			head = head.next;
+			head.next = prev;
+			prev = head;
+		}
+		return curr;
+	}
+}
+//leetcode submit region end(Prohibit modification and deletion)
